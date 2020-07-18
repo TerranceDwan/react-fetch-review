6d9b2006aa38eae0a0c6e64c7e5a3593ff22e5b2
Terrance Lewis <terrance@Jordans-MacBook-Pro.local>
2020-07-18 15:17:46 -0400

    Added Bootstrap Classes

--- src/JokeButton.js
+++ src/JokeButton.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Test(){
     return (
         <div>
-            <button id="button" onClick={jokeProduction}>
+            <button className="button btn-outline-info btn-lg my-4" id="button" onClick={jokeProduction}>
                 <h2>
                     Programming Jokes
                 </h2>
             </button>
-            <p id="setup"></p>
+            <p className="m2" id="setup"></p>
             <p id="delivery"></p>
         </div>
     );
